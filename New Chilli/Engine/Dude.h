e6161b63669095cf3e56742e34e5bb5c3746eb0d
Anumey Sharma <anumeysharma_shashidhar@srmuniv.edu.in>
2018-07-05 17:21:27 +0530

    Code Upload

---
+++ New Chilli/Engine/Dude.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class Dude
+{
+public:
+	
+	void ClampToScreen();
+	
+	
+	int x = 400;
+	int y = 300;
+	static constexpr int width = 24;
+	static constexpr int height = 24;
+
+
+
+};
